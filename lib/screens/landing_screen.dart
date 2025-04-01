203cfc3b089534ec338feb08ab10834cf8c1a65b
Paulo Benitez <paulosantibd@gmail.com>
2025-03-31 21:52:12 -0300

    folder structure improvements

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/home_screen.dart';
+import 'package:chat_app/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:swipeable_button_view/swipeable_button_view.dart';
