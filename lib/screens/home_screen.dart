46c5fc6c6bc427790cdee225eeecd1013969ad80
Paulo Benitez <paulosantibd@gmail.com>
2025-04-02 00:27:39 -0300

    implementacion de go router

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/screens/chat_details_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,12 +11,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   List<String> people = [
     'Colt',
     'Mayra',
     'Neeta',
-    'Sarah'
+    'Sarah',
     'Natasha',
     'Robert',
     'Thomas',
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
     'Olivia',
     'Emma',
     'Amelia',
-    'Liam'
+    'Liam',
     'William',
     'Lucas',
     'Henry',
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
@@ -48,17 +48,13 @@ class _HomeScreenState extends State<HomeScreen> {
             Text(
               "Hello Paulo,",
               style: TextStyle(
-                color: Colors.grey,
-                fontWeight: FontWeight.w300,
-                fontSize: 12
-
-              ),
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 12),
             ),
-             Text(
+            Text(
               "ChatApp message",
-              style: TextStyle(
-                color: Colors.black
-              ),
+              style: TextStyle(color: Colors.black),
             ),
           ],
         ),
@@ -66,59 +62,51 @@ class _HomeScreenState extends State<HomeScreen> {
           Padding(
             padding: const EdgeInsets.only(right: 8.0),
             child: IconButton(
-              onPressed: (){}, 
-              icon: Icon(
-                Icons.edit_square,
-                color: Colors.grey,
-              )
-              ),
+                onPressed: () {},
+                icon: Icon(
+                  Icons.edit_square,
+                  color: Colors.grey,
+                )),
           ),
         ],
-        
       ),
-      body:SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-                child: ListView.builder(
-                  itemCount: people.length,
-                  shrinkWrap: true,
-                  //physics: const NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  //itemCount: people.length,
-                  itemBuilder: (context, int i) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                               color: Colors.grey.shade200,
-                               image: DecorationImage(
-                                image: AssetImage(
-                                'assets/${i+1}.png'
-                                ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            height: 100,
+            child: ListView.builder(
+                itemCount: people.length,
+                //shrinkWrap: true,
+                //physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                //itemCount: people.length,
+                itemBuilder: (context, int i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              image: DecorationImage(
+                                image: AssetImage('assets/${i + 1}.png'),
                                 scale: 10,
-                                ),
-                               shape: BoxShape.circle
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            people[i]
-                          )
-                        ],
-                      ),
-                    );
-                  }
-                  ),
-              ),
-             const Divider(),
-             ListView.builder(
+                              ),
+                              shape: BoxShape.circle),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(people[i])
+                      ],
+                    ),
+                  );
+                }),
+          ),
+          const Divider(),
+          ListView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               itemCount: people.length,
@@ -126,16 +114,15 @@ class _HomeScreenState extends State<HomeScreen> {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ListTile(
-                    onTap: (){
-                      Navigator.push(
-                    context, 
-                    MaterialPageRoute<void>(
-                      builder: (BuildContext context) => ChatDetailsScreen(
-                        index + 1,
-                        people[index]
-                      ),
-                      ),
-                  );
+                    onTap: () {
+                      // context.push("/chat-details");
+                      context.pushNamed(
+                        'chat-details',
+                        extra: ChatDetailsExtra(
+                          index: index,
+                          name: people[index],
+                        ),
+                      );
                     },
                     leading: Container(
                       decoration: BoxDecoration(
@@ -144,71 +131,60 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       child: Padding(
                         padding: const EdgeInsets.all(6.0),
-                        child: Image.asset(
-                          "assets/${index+1}.png"
-                          ),
+                        child: Image.asset("assets/${index + 1}.png"),
                       ),
                     ),
                     title: Text(
                       people[index],
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    subtitle: Text(
-                      "blablablabla...",
-                      //style: const TextStyle(
-                       // fontWeight: FontWeight.bold
-                      ),
-                      trailing: Column(
-                        children: [
-                          Text(
-                            '00.21',
-                            style: TextStyle(
-                              fontSize: 10,
-                              color: Colors.grey
+                    subtitle: Text("blablablabla..."),
+                    //style: const TextStyle(
+                    // fontWeight: FontWeight.bold
+
+                    trailing: Column(
+                      children: [
+                        Text(
+                          '00.21',
+                          style: TextStyle(fontSize: 10, color: Colors.grey),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(
+                              CupertinoIcons.pin_fill,
+                              size: 15,
+                              color: Colors.grey,
                             ),
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                             const Icon(
-                                CupertinoIcons.pin_fill,
-                                size: 15,
-                                color: Colors.grey,
+                            SizedBox(width: 5),
+                            Container(
+                              width: 15,
+                              height: 15,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue,
                               ),
-                              SizedBox(width: 5),
-                              Container(
-                                width: 15,
-                                height: 15,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.blue,
-                                ),
-                                child: const Center(
-                                  child: Text(
-                                    '1',
-                                    style: TextStyle(
+                              child: const Center(
+                                child: Text(
+                                  '1',
+                                  style: TextStyle(
                                       color: Colors.white,
                                       fontSize: 9,
-                                      fontWeight: FontWeight.bold
-                                    ),
-                                  ),
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ),
-                            ],
-                          )
-                        ],
-                      ),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                 // ),
+                  ),
+                  // ),
                 );
-              }
-             )
-          ],
-        )
-      ) ,
+              })
+        ],
+      )),
     );
   }
 }
--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,8 +1,8 @@
 import 'package:chat_app/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:swipeable_button_view/swipeable_button_view.dart';
-
 
 class LandingScreen extends StatefulWidget {
   const LandingScreen({super.key});
@@ -12,11 +12,10 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
-
   late bool isFinished;
 
   @override
-  void initState(){
+  void initState() {
     isFinished = false;
     super.initState();
   }
@@ -29,84 +28,58 @@ class _LandingScreenState extends State<LandingScreen> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/background.png'
-                  ),
-                  fit: BoxFit.cover,
-                )
-              ),
-            ),
-            Container(
-              height:  MediaQuery.of(context).size.height /2.4,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(30)
-                )
-              ),
-              child:  Padding(
-                padding: const EdgeInsets.all(20.0),
-                child:  Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                   const Text(
-              "Express yourself with ChatApp",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w600
-              ),
-              ),
-             const Text(
-              "Chat using avatar emoji gives a diferent, dare to try it ?",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.w300
-              ),
-              ),
-              SwipeableButtonView(
-                isFinished: isFinished,
-                onFinish: (){
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute<void>(
-                      builder: (BuildContext context) => const HomeScreen(),
-                      ),
-                  );
-                }, 
-                onWaitingProcess: (){
-                  setState(() {
-                    isFinished = true;
-                  });
-                },
-                activeColor: Colors.blue,
-                buttonWidget: Icon(
-                  CupertinoIcons.chevron_right_2,
-                  color: Colors.grey,
+      body: Stack(alignment: Alignment.bottomCenter, children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.cover,
+          )),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height / 2.4,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(30))),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  "Express yourself with ChatApp",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
                 ),
-                buttonText: "Swipe to start"
-                )
-                  ], 
+                const Text(
+                  "Chat using avatar emoji gives a diferent, dare to try it ?",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w300),
                 ),
-              ),
+                SwipeableButtonView(
+                    isFinished: isFinished,
+                    onFinish: () {
+                      context.pushNamed('home');
+                    },
+                    onWaitingProcess: () {
+                      setState(() {
+                        isFinished = true;
+                      });
+                    },
+                    activeColor: Colors.blue,
+                    buttonWidget: Icon(
+                      CupertinoIcons.chevron_right_2,
+                      color: Colors.grey,
+                    ),
+                    buttonText: "Swipe to start")
+              ],
             ),
-            
-        ]
-        
-          
-        
-      ),
-
+          ),
+        ),
+      ]),
     );
   }
 }
